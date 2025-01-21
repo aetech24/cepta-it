eda2a6446d11dde038ab8a8fe4fb16d72a403067
Stedan1234 <aetech.hub@aetechorg.com>
2025-01-21 18:38:03 +0000

    Refactor footer and Home page layout for improved styling and structure
    - Removed unnecessary top margin from the footer component for a cleaner look.
    - Reformatted the Home page layout for better organization and readability, including consistent spacing for product sections.
    - Enhanced the ProductGrid component usage to ensure a uniform grid layout across different product categories.
    - Improved code readability by restructuring import statements and component organization.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,7 +11,12 @@ import products from '../constants/products';
 import leftBanner from '../assets/ps5-banner.png';
 import rightTopBanner from '../assets/laptop-banner.png';
 import rightBottomBanner from '../assets/jbl-banner.png';
-import { PiTruck, PiHandCoinsThin, PiHeadset, PiShieldCheck } from 'react-icons/pi';
+import {
+  PiTruck,
+  PiHandCoinsThin,
+  PiHeadset,
+  PiShieldCheck,
+} from 'react-icons/pi';
 
 const trendingProducts = products.filter(
   (product) => product.type === 'trending'
@@ -42,19 +47,22 @@ const Home = () => {
         </div>
       </div>
       <div className='w-[100%] flex flex-col'>
-        <div className='flex flex-row justify-between items-center justify-self-center pt-24 mx-auto w-[100%] px-14'>
-          <div className=''>
-            <h4 className='text-black text-2xl font-[400] text-start relative'>
-              Trending Products
-              <span className='block h-[2px] bg-[#EF0303] hover:bg-[#00278C] transition-all duration-300 w-1/2 absolute top-full left-0 mt-1 font-[2px] drop-shadow-xl'></span>
-            </h4>
-          </div>{' '}
-          <button className='bg-none border-[1.4px] border-black text-black px-4 py-2 hover:bg-black hover:text-white transition-all duration-300'>
-            View All
-          </button>
+          <div className='flex flex-row justify-between items-center justify-self-center pt-24 mx-auto w-[100%] px-14'>
+            <div className=''>
+              <h4 className='text-black text-2xl font-[400] text-start relative'>
+                Trending Products
+                <span className='block h-[2px] bg-[#EF0303] hover:bg-[#00278C] transition-all duration-300 w-1/2 absolute top-full left-0 mt-1 font-[2px] drop-shadow-xl'></span>
+              </h4>
+            </div>{' '}
+            <button className='bg-none border-[1.4px] border-black text-black px-4 py-2 hover:bg-black hover:text-white transition-all duration-300'>
+              View All
+            </button>
+          </div>
+          <ProductGrid
+            products={trendingProducts}
+            className='grid grid-cols-1 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-8 px-4 pt-10 mx-auto max-w-[100%]'
+          />
         </div>
-        <ProductGrid products={trendingProducts} />
-      </div>
       <div className='flex flex-row bg-[#E6EAF5] w-[1240px] h-[451px] max-sm:w-[100%] justify-self-center justify-between mx-auto mt-10 px-36'>
         <div className='flex flex-col justify-center items-start gap-4'>
           <p className='text-[#0E0F0F] text-sm'>Apple Deals</p>
@@ -127,7 +135,10 @@ const Home = () => {
               View All
             </button>
           </div>
-          <ProductGrid products={newProducts} />
+          <ProductGrid
+            products={newProducts}
+            className='grid grid-cols-1 sm:grid-cols-4 gap-8 px-4 pt-10 mx-auto max-w-[100%]'
+          />
         </div>
         <div className='flex justify-between items-center pt-24 mx-auto mb-10 w-[100%] h-[600px] px-14 gap-6'>
           {/* Left Banner */}
@@ -193,37 +204,37 @@ const Home = () => {
             </div>
           </div>
         </div>
-          <div className='flex flex-row justify-center items-center mx-auto w-[100%] h-[344px] my-auto px-14 gap-10  bg-[#FDE6E6]'>
-            <div className='flex flex-row items-center gap-4'>
-              <PiTruck className='text-[#EF0303] w-[56px] h-[56px]' />
-              <div>
-                <p>Free Delivery</p>
-                <span>For Orders Over $100</span>
-              </div>
+        <div className='flex flex-row justify-center items-center mx-auto w-[100%] h-[344px] my-auto px-14 gap-10  bg-[#FDE6E6]'>
+          <div className='flex flex-row items-center gap-4'>
+            <PiTruck className='text-[#EF0303] w-[56px] h-[56px]' />
+            <div>
+              <p>Free Delivery</p>
+              <span>For Orders Over $100</span>
             </div>
-            <div className='flex flex-row items-center gap-4'>
-              <PiHandCoinsThin className='text-[#EF0303] w-[56px] h-[56px]' />
-              <div>
-                <p>Money Back Guarantee</p>
-                <span>Full refund guaranteed</span>
-              </div>
+          </div>
+          <div className='flex flex-row items-center gap-4'>
+            <PiHandCoinsThin className='text-[#EF0303] w-[56px] h-[56px]' />
+            <div>
+              <p>Money Back Guarantee</p>
+              <span>Full refund guaranteed</span>
             </div>
-            <div className='flex flex-row items-center gap-4'>
-              <PiHeadset className='text-[#EF0303] w-[56px] h-[56px]' />
-              <div>
-                <p>24/7 Support</p>
-                <span>We are always here for you</span>
-              </div>
+          </div>
+          <div className='flex flex-row items-center gap-4'>
+            <PiHeadset className='text-[#EF0303] w-[56px] h-[56px]' />
+            <div>
+              <p>24/7 Support</p>
+              <span>We are always here for you</span>
             </div>
-            <div className='flex flex-row items-center gap-4'>
-              <PiShieldCheck className='text-[#EF0303] w-[56px] h-[56px]' />
-              <div>
-                <p>Secure Payment</p>
-                <span>100% secure payment</span>
-              </div>
+          </div>
+          <div className='flex flex-row items-center gap-4'>
+            <PiShieldCheck className='text-[#EF0303] w-[56px] h-[56px]' />
+            <div>
+              <p>Secure Payment</p>
+              <span>100% secure payment</span>
             </div>
           </div>
         </div>
+      </div>
     </main>
   );
 };
